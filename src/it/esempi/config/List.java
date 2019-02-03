c0111484ab4d291b7656fb1b3fc5eafaa04dbe73
vincenzo di franco <vincenzo.difranco@gmail.com>
2019-02-03 22:14:58 +0100

    list html
    esempio list html per servlet, uso di annotation in List.java

---
+++ src/it/esempi/config/List.java
@@ -0,0 +1,42 @@
+package it.esempi.config;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class List
+ */
+@WebServlet({ "/list", "/list2" })
+public class List extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public List() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String forwardPath ="/WEB-INF/pages/list.jsp";
+		getServletContext().getRequestDispatcher(forwardPath).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
